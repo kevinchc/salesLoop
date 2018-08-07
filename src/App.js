515f5ec03c02c3aa8fae886feec774a4e3e781d7
@@4787chagua <chaguakevin@gmail.com>
2018-08-07 15:41:41 -0500

    new application in react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MenuAppBar from './components/nav/nav';
+
+class App extends Component {
+  render() {
+    return (
+      <MenuAppBar/>
+    );
+  }
+}
+
+export default App;
